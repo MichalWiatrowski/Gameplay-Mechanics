f0470df1a3cebbdb98d541b97fa8c4c6b3632550
Michal Wiatrowski <1600597@uad.ac.uk>
2019-04-12 16:15:54 +0100

    Recovered from project corruption
    Fixed wall climbing and disabled the debug sphere on sonic arrow as it caused massive lag

--- GameplayMechanics/Intermediate/Build/Win64/GameplayMechanicsEditor/Development/Engine/SharedPCH.Engine.cpp
+++ GameplayMechanics/Intermediate/Build/Win64/GameplayMechanicsEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Michal/Documents/GitHub/Gameplay-Mechanics/GameplayMechanics/Intermediate/Build/Win64/GameplayMechanicsEditor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Michal/Desktop/IDIE/Gameplay-Mechanics/GameplayMechanics/Intermediate/Build/Win64/GameplayMechanicsEditor/Development/Engine/SharedPCH.Engine.h"
